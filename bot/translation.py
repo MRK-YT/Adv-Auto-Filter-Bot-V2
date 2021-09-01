6efa60317ae38cca3e4e15176d271d31de49e3f8
Filterbot4882 <89912454+Filterbot4882@users.noreply.github.com>
2021-09-01 22:05:02 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,24 +5,17 @@
 class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
-Hi bro sugaanno
- """    
+     Iam bot of Film zone groups
+"""    
     
-    HELP_TEXT = """
-😊😉
-"""
+    
     
     ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
     
-<b>➥ 😎Creator</b> : <b>@AlbertEinstein_TG</b> 
+<b>➥ Creator</b> : <b>@jasimmhd</b> 
 
-<b>➥ 👨‍💻Editor</b> : <b>@Mrk_YT</b>
+<b>➥ Language</b> : <b>Python3</b>
 
-<b>➥ 🗣️Language</b> : <b>Python3</b>
+<b>➥ channel</b> : <b><a href="https://t.me/filmzone_123"></a>💥Click Me</b>
 
-<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot">💥Click Me</a></b>
-
-Add 🥳
 """
