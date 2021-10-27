873e56b91e91bd5dcc1f178af4d881406f38252e
Anshidrah <91682595+Anshidrah@users.noreply.github.com>
2021-10-27 21:30:25 +0530

    translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,16 +5,8 @@
 class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
-
-<b>Am Just A Pro Auto Filter Bot....😉</b>
-
-<b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
-
-<b>Watch the video below to build an Pro Autofilter bot like me...</b>
-
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>"""    
+Hi sugamalle 
+"""    
     
     HELP_TEXT = """
 <b>😉How To Use Me!😉?</b>
